1ba2a146e03795197de634d576e5c8176a6766c6
nour <nmetwall@ualberta.ca>
2025-10-18 17:41:56 -0600

    base

---
+++ code/app/src/main/java/com/example/beethere/ui/home/HomeFragment.java
@@ -0,0 +1,27 @@
+package com.example.beethere.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+
+import com.example.beethere.R;
+
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        /* TODO
+         * write code here
+         */
+
+        return view;
+    }
+}
