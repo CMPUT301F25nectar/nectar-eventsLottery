2295f20b1a6f1e2a98c8d7f1eb95a9580306cb0b
nour <nmetwall@ualberta.ca>
2025-11-06 15:31:11 -0700

    hana pulling

--- code/app/src/main/java/com/example/beethere/eventclasses/Event.java
+++ code/app/src/main/java/com/example/beethere/eventclasses/Event.java
@@ -1,17 +1,20 @@
-package com.example.beethere;
+package com.example.beethere.eventclasses;
+
+import com.example.beethere.User;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
 
 public class Event {
     private User organizer;
-    private String title;
+    private String name;
     private String description;
-    private int image;
+
+    private int poster;
     private int qrCode;
 
     private Boolean status;
+
 
     private LocalDate regStart;
     private LocalDate regEnd;
@@ -22,52 +25,131 @@ public class Event {
     private LocalTime eventTimeEnd;
 
 
-    private int entrantMax;
-
     private Boolean geoloc;
-
-    //move these 2 attributes into waitlist manager class, make into 1 attribute
-    // that class would control the display of number of people in waitlist
-    // of adding and removing from waitlist
-    // of randomly selecting from waitlist
-    // etc?
-
-    private ArrayList<User> waitlist;
-    private int waitlistMax;
-
-
-    private ArrayList<User> inviteList;
-    private ArrayList<User> registered;
-
     private Boolean autoRandomSelection;
 
-    // all have getters and setters
-
-    // Display lists are handled by the fragment
-
-    // Random selection from waitlist into invite
-    // includes sending notification
-
-    // export registered as CSV
-
-
-    // NEW CLASS???
-    // EventDetailsAcvitiy class
-
-
-    // addtowaitlist
-    // includes checking for max
-
-
-    // add to entrants
-    // includes checking for max
-
-    // autoRandomSelection
-    // how to check declining invite
-    // if there are multiple people declining what happens?
+    private UserListManager listManager;
 
 
 
 
+    public User getOrganizer() {
+        return organizer;
+    }
 
+    public void setOrganizer(User organizer) {
+        this.organizer = organizer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPoster() {
+        return poster;
+    }
+
+    public void setPoster(int poster) {
+        this.poster = poster;
+    }
+
+    public int getQrCode() {
+        return qrCode;
+    }
+
+    public void setQrCode(int qrCode) {
+        this.qrCode = qrCode;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public LocalDate getRegStart() {
+        return regStart;
+    }
+
+    public void setRegStart(LocalDate regStart) {
+        this.regStart = regStart;
+    }
+
+    public LocalDate getRegEnd() {
+        return regEnd;
+    }
+
+    public void setRegEnd(LocalDate regEnd) {
+        this.regEnd = regEnd;
+    }
+
+    public LocalDate getEventDateStart() {
+        return eventDateStart;
+    }
+
+    public void setEventDateStart(LocalDate eventDateStart) {
+        this.eventDateStart = eventDateStart;
+    }
+
+    public LocalDate getEventDateEnd() {
+        return eventDateEnd;
+    }
+
+    public void setEventDateEnd(LocalDate eventDateEnd) {
+        this.eventDateEnd = eventDateEnd;
+    }
+
+    public LocalTime getEventTimestart() {
+        return eventTimestart;
+    }
+
+    public void setEventTimestart(LocalTime eventTimestart) {
+        this.eventTimestart = eventTimestart;
+    }
+
+    public LocalTime getEventTimeEnd() {
+        return eventTimeEnd;
+    }
+
+    public void setEventTimeEnd(LocalTime eventTimeEnd) {
+        this.eventTimeEnd = eventTimeEnd;
+    }
+
+    public Boolean getGeoloc() {
+        return geoloc;
+    }
+
+    public void setGeoloc(Boolean geoloc) {
+        this.geoloc = geoloc;
+    }
+
+    public Boolean getAutoRandomSelection() {
+        return autoRandomSelection;
+    }
+
+    public void setAutoRandomSelection(Boolean autoRandomSelection) {
+        this.autoRandomSelection = autoRandomSelection;
+    }
+
+    public UserListManager getListManager() {
+        return listManager;
+    }
+
+    public void setListManager(UserListManager listManager) {
+        this.listManager = listManager;
+    }
 }
